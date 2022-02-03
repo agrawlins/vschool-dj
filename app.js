d05bff593c29735be4460e045f5f2731252f4f1e
Adam Rawlins <agrawlins@gmail.com>
2022-02-02 19:07:58 -0500

    figured out the mouse controls! Now, to get the keyboard commands ironed out...

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-var x = document.getElementById("myBtn");
-x.addEventListener("mouseover", hover);
-x.addEventListener("mousedown", holdClick);
-x.addEventListener("mouseup", releaseClick);
-x.addEventListener("dblclick", doubleClick);
-addEventListener("mousewheel", mouseWheel);
-addEventListener("keypress", keyPress);
-
+document.addEventListener("mouseover", hover);
+document.addEventListener("mousedown", hold);  
+document.addEventListener("mouseup", release);
+document.addEventListener("dblclick", dblClick);  
+document.addEventListener("wheel", scroll); 
 function hover() {
-document.getElementById("demo").innerHTML = "Moused Over!";
+    document.getElementById("colorBox").innerHTML = "Moused over!";
+    document.body.style.backgroundColor = "blue";
 }
-
-function holdClick() {
-    document.getElementById("demo").innerHTML = "Holding!";
+function hold() {
+    document.getElementById("colorBox").innerHTML = "Holding...";
+    document.body.style.backgroundColor = "red";
 }
-
-function releaseClick() {
-    document.getElementById("demo").innerHTML = "Released!";
+function release() {
+    document.getElementById("colorBox").innerHTML = "Released!";
+    document.body.style.backgroundColor = "green";
 }
-
-function doubleClick() {
-    document.getElementById("demo").innerHTML = "Double-Clicked!";
+function dblClick() {
+    document.getElementById("colorBox").innerHTML = "Double-Clicked!";
+    document.body.style.backgroundColor = "purple";
 }
-
-function mouseWheel() {
-    document.getElementById("demo").innerHTML = "Scrolling!";
+function scroll() {
+    document.getElementById("colorBox").innerHTML = "Scrolling!";
+    document.body.style.backgroundColor = "orange";
 }
-
 function keyPress() {
-    
+    let txt = document.getElementById('txt');
+    txt.addEventListener('keydown')
 }
