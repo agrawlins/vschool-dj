68b36f371ae46bc0b864cb9714a04798688ad2d0
Adam Rawlins <agrawlins@gmail.com>
2022-02-03 18:13:11 -0500

    Finished product!

--- app.js
+++ app.js
@@ -2,28 +2,26 @@ document.addEventListener("mouseover", hover);
 document.addEventListener("mousedown", hold);  
 document.addEventListener("mouseup", release);
 document.addEventListener("dblclick", dblClick);  
-document.addEventListener("wheel", scroll); 
+document.addEventListener("wheel", scroll);  
+
 function hover() {
     document.getElementById("colorBox").innerHTML = "Moused over!";
-    document.body.style.backgroundColor = "blue";
+    document.getElementById("colorBox").style.backgroundColor = "blue";
 }
 function hold() {
     document.getElementById("colorBox").innerHTML = "Holding...";
-    document.body.style.backgroundColor = "red";
+    document.getElementById("colorBox").style.backgroundColor = "red";
 }
 function release() {
     document.getElementById("colorBox").innerHTML = "Released!";
-    document.body.style.backgroundColor = "green";
+    document.getElementById("colorBox").style.backgroundColor = "green";
 }
 function dblClick() {
     document.getElementById("colorBox").innerHTML = "Double-Clicked!";
-    document.body.style.backgroundColor = "purple";
+    document.getElementById("colorBox").style.backgroundColor = "purple";
 }
 function scroll() {
     document.getElementById("colorBox").innerHTML = "Scrolling!";
-    document.body.style.backgroundColor = "orange";
+    document.getElementById("colorBox").style.backgroundColor = "orange";
 }
-function keyPress() {
-    let txt = document.getElementById('txt');
-    txt.addEventListener('keydown')
-}
+
